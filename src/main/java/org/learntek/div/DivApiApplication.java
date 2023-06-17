c1b64eb173a401241d2e487a4bbcd3f328fbfe47
Binayak Nanda <binayak.nanda@gmail.com>
2023-06-17 23:45:40 +0530

    First Commit

---
+++ src/main/java/org/learntek/div/DivApiApplication.java
@@ -0,0 +1,15 @@
+package org.learntek.div;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DivApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivApiApplication.class, args);
+	}
+
+}
